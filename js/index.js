870a19b627018d4661ff687eb482e520170c662e
yangtao <1509440167@qq.com>
2019-04-23 01:18:01 +0800

    111

---
+++ js/index.js
@@ -0,0 +1,27 @@
+﻿(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
+
+$(document).ready(function () {
+});
+//显示遮罩层
+function showMask() {
+    $("#mask").css("height", $(document).height());
+    $("#mask").css("width", $(document).width());
+    $("#mask").show();
+    $('body').css('position', 'fixed');
+}
+//隐藏遮罩层
+function hideMask() {
+    $("#mask").hide();
+    $('body').css('position', 'unset');
+}
---
+++ js/main.js
@@ -0,0 +1,50 @@
+﻿$(document).ready(function(){	
+	var target = document.getElementById('province1');
+	// debugger
+	if (target.addEventListener) {
+		target.addEventListener('change', selectedHandler, false);
+	} else if (target.attachEvent) {
+		target.attachEvent('change', selectedHandler);
+	}
+	maphover();
+});
+
+function selectedHandler(){	
+	if($("#province1")[0].value!='')
+	{
+		var selectedText = $("#province1")[0].value;
+		$(".city").each(function(){
+			if ($(this).children('div').attr('data') == selectedText) {
+				$(this).children('div').show()
+			} else {
+				$(this).children('div').hide()
+			}
+		})
+	}			
+};
+
+function maphover(){	
+	var self = "";
+	$(".city").hover(
+		function(){
+			self = $(this);
+			self.addClass("hover").children("div").show();
+		},
+		function(){
+			self = $(this);
+			self.children("div").hide();
+			self.removeClass("hover");
+		}
+	);
+	$(".city").click(
+		function(){
+			self = $(this);
+			self.addClass("hover").children("div").show();
+		},
+		function(){
+			self = $(this);
+			self.children("div").hide();
+			self.removeClass("hover");
+		}
+	);		
+};
